4a221f10f47908375995e606f226552a47b726d5
Joel Mathias <joelmathias.forever9@gmail.com>
2024-08-05 03:11:01 -0300

    Criando a Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import "./Navbar.css"
+
+export default function Navbar() {
+    return (
+        <header>
+            <h1>Imagem</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">
+                            <i></i>
+                            <label htmlFor="">Projetos</label>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i></i>
+                            <label htmlFor="">Parcerias</label>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i></i>
+                            <label htmlFor="">Sobre</label>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i></i>
+                            <input type="search" name="" id="" />
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
